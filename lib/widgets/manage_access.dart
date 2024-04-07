dad11891238f5e02a606a3ff6a9492191d0f8a08
Alejandro Wang <awang21913@gmail.com>
2024-04-07 00:21:21 -0400

    initial commit

---
+++ lib/widgets/manage_access.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+/// The class will be used to create a manage access 
+/// modal page, which will be used to update permissions for a given file.
+class ManageAccess extends StatelessWidget {
+  /// Creates app instance of the ManageAccess modal page.
+  /// 
+  /// The modal page will control the sharing features.  
+  /// Thus, it will have a text box to enter a user's email,
+  /// along with a scroll-down menu to give them either read
+  /// or write access.
+  /// 
+  /// params: [BuildContext] context
+  /// return: [MaterialApp] widget
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
+
+/// Creates the the instance of the modal page
+class ManageAccessPage extends StatelessWidget {
+  /// Create structure/layout of the page.
+  /// This includes a textbox, a scroll-down menu,
+  /// and a button.  
+  /// 
+  /// params: [BuildContext] context
+  /// returns: [Scaffold] widget
+  @override
+  Widget build(Build context) {
+    returns Scaffold();
+  }
+
+}
