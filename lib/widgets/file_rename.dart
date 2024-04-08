049e8d6c6c7225afd470a230809e5500792a955c
hwoj <hwojnicki41@gmail.com>
2024-04-07 21:29:14 -0700

    partway through designs

--- lib/widgets/file_rename.dart
+++ lib/widgets/file_rename.dart
@@ -34,14 +34,24 @@ class FileRenameState extends State<FileRename> {
   /// This function checks to see if the [newName] follows the allowed file naming conventions and 
   /// returns a boolean that provides indication of if this is the case or not 
   bool validateName(String newName) {
+    // makesure that the newName string contains valid filename characters (using regex)
 
+    // return whether this is the case or not
   }
 
   /// This is the function reponsible for renaming the file given the user input
   ///
   /// It will change the name of the file after validating it with [validateName]
   void saveName(String newName) {
+    // call validateName to make sure the name is correct
 
+    // if validated then update name of file object locally
+    // push change to server
+    // close modal and widget
+
+    // if not validated then show error message and keep modal open
+
+    // return
   }
 
   /// The function reponsible for closing the FileRename
@@ -49,11 +59,16 @@ class FileRenameState extends State<FileRename> {
   /// This function will handle the logic for the FileRename widget being closed if the user doesn't 
   /// want to proceed with renaming the file
   void cancelRename() {
-
+    // close modal and widget
   }
 
+  /// The build function contains the contents of the state
+  ///
+  /// this function will handle the creation and recreation of the state
   @override
   Widget build(BuildContext context) {
+    // will contain a dialog with a text input area and 2 buttons
+    // 1 button for closing the widget, another for submitting the rename
     return Dialog();
   }
 }
