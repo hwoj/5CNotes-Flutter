18f6280fdb77f13d9b0b9e3bbb7fbe740d5719ba
hwoj <hwojnicki41@gmail.com>
2024-04-10 10:32:52 -0700

    basic db initialization done

---
+++ lib/services/api_services/database_service.dart
@@ -0,0 +1,47 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+/// This class controls communications with the local sqlite db
+///
+/// This class is responsible for initializing and facilitating communications to the database
+class DatabaseService {
+  static Database? _database;
+
+  /// This function returns an initialized database
+  ///
+  /// If the database is already initialized it just returns it, otherwise it initializes it then returns it
+  Future<Database> get database async {
+    if (_database != null) {
+      return _database!;
+    }
+
+    _database = await initDatabase();
+    return _database!;
+  }
+
+  /// This function initializes the database
+  ///
+  /// Called in [database] this function creates the table and initializes the database 
+  /// in the case that it wasn't initialized yet
+  Future<Database> initDatabase() async {
+    String path = join(await getDatabasesPath(), "fileinfo.db");
+
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: (Database db, int version) async {
+        await db.execute('''
+          CREATE TABLE files(
+            id INTEGER PRIMARY KEY,
+            uuid TEXT,
+            name TEXT,
+            author TEXT,
+            createdAt TEXT,
+            lastEdited TEXT,
+            course TEXT,
+            userCount INTEGER,           
+          )
+        ''');
+      }
+    );
+  }
+}
