232d1533e4be9d4e6f59c5a914ffef4cfc514233
hwoj <hwojnicki41@gmail.com>
2024-04-28 15:32:51 -0700

    dark theme toggle works

--- lib/main.dart
+++ lib/main.dart
@@ -2,31 +2,53 @@
 import 'package:fivec_notes/screens/login_screen.dart';
 import 'package:fivec_notes/themes/app_colors_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
 void main() {
   runApp(const MainApp());
 }
 
+
+
+class ThemeNotifier extends ChangeNotifier {
+  ThemeMode _themeMode = ThemeMode.light;
+  ThemeMode get themeMode => _themeMode;
+
+  void toggleTheme() {
+    _themeMode = _themeMode == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
+    notifyListeners();
+  }
+}
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return ChangeNotifierProvider(
+      create: (_) => ThemeNotifier(),
+      child: Consumer(
+        builder: (BuildContext context, ThemeNotifier themeNotifier, _) {
+          return MaterialApp(
 
-      home: LoginScreen(),
-      theme: Theme.of(context).copyWith(
-        extensions: [
-          AppColorsTheme.light()
-        ]
-      ),
-      darkTheme: Theme.of(context).copyWith(
-        extensions: [
-          AppColorsTheme.dark()
-        ]
-      ),
+            home: LoginScreen(),
+            themeMode: themeNotifier.themeMode,
+            theme: Theme.of(context).copyWith(
+              extensions: [
+                AppColorsTheme.light()
+              ]
+            ),
+            darkTheme: Theme.of(context).copyWith(
+              extensions: [
+                AppColorsTheme.dark()
+              ]
+            ),
+          );
+        }
+        )
     );
   }
 }
+
 
 /// Exposing the app themes to all files
 extension ThemeDataExtended on ThemeData {
