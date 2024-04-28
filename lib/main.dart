bda339692b032152b97f258652a413875ba02aba
Alejandro Wang <awang21913@gmail.com>
2024-04-28 17:15:45 -0400

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,12 @@ import 'package:fivec_notes/themes/theme_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 void main() {
-  runApp(const MainApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      child: const MainApp(),
+      )
+  );
 }
 
 class MainApp extends StatelessWidget {
---
+++ tests/widget_tests/login_screen_test.dart
@@ -0,0 +1,15 @@
+import 'package:fivec_notes/screens/login_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets("widget", (tester) async {
+    await tester.pumpWidget(LoginScreen());
+
+    final email_finder = find.byIcon(TextEditingController())
+  }
+  );
+  
+
+}
+
